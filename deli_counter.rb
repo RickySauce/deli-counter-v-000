65e55f7492183cdc1de928c980e1fe67e8bcf987
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 17:11:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(deli)
     if deli.size == 0
       puts "The line is currently empty."
     else
-      puts deli.size
+      deli.each_with_index do |customers,index|
+        index = deli[index] + 1
     end
 end
