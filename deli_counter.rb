a5ae5ab63b20d9b00aee4fe2423c0bede377bc18
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 17:21:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(deli)
         combination = "#{number}. #{customers}"
         total << combination
       end
-      total.join " "
+      total.join 
       puts "The line is currently: #{total}"
     end
 end
