4bb48a51e8062a9c07c5793e37f6691df9b8775e
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 17:08:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ deli = Array.new
 
 def line(deli)
     if deli.size == 0
-      puts "the line is currently empty."
+      puts "The line is currently empty."
     else
       puts deli.size
     end
