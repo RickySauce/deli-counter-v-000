0a4196264e8052099b5abb8cc7f452862e3c1ea7
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 17:21:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,13 @@ def line(deli)
     if deli.size == 0
       puts "The line is currently empty."
     else
+      total = Array.new
       deli.each_with_index do |customers,index|
         number = index + 1
-        puts "The line is currently: #{number}. #{customers}" << "#{number}. #{customers}"
+        combination = "#{number}. #{customers}"
+        total << combination
       end
+      total.join " "
+      puts "The line is currently: #{total}"
     end
 end
